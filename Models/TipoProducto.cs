b0f708f6aef6fd42ba7bf583d186476c140c8d00
AngelixVR <Jose Gabriel@DESKTOP-SQIUKTF>
2025-08-11 10:37:53 -0400

    Add TipoProducto class

---
+++ Models/TipoProducto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VRJewelers.Models;
+
+public class TipoProducto
+{
+    [Key]
+    public int TipoId { get; set; }
+
+    [Required(ErrorMessage = "No puede estar Vacio")]
+    [RegularExpression("^[a-zA-Z ]+$", ErrorMessage = "El Nombre debe contener solo letras.")]
+    public string? Nombre { get; set; }
+}
