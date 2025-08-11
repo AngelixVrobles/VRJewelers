25cf87efc0040177f0d42557b64b33920dd85c41
AngelixVR <Jose Gabriel@DESKTOP-SQIUKTF>
2025-08-11 10:33:20 -0400

    Add Estados class

---
+++ Models/Estados.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VRJewelers.Models;
+
+public class Estados
+{
+    [Key]
+    public int EstadoId { get; set; }
+
+    [Required(ErrorMessage = "El nombre del Estado es obligatorio")]
+    public string? NombreEstado { get; set; }
+}
