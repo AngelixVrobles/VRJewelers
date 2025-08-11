ade48b2adeb24a5c6b4a61192bd6d5031fbcc202
AngelixVR <Jose Gabriel@DESKTOP-SQIUKTF>
2025-08-11 13:14:35 -0400

    Add new services class

---
+++ Services/EstadosService.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq.Expressions;
+using VRJewelers.Data;
+using VRJewelers.Models;
+namespace VRJewelers.Services;
+
+public class EstadosService(IDbContextFactory<ApplicationDbContext> contextFactory) : Estados
+{
+    private readonly IDbContextFactory<ApplicationDbContext> _contextFactory = contextFactory;
+
+    public async Task<List<Estados>> GetAllObject()
+    {
+        using var context = _contextFactory.CreateDbContext();
+        return await context.Estados.ToListAsync();
+    }
+
+    public async Task<Estados> GetObject(int id)
+    {
+        using var context = _contextFactory.CreateDbContext();
+        return await context.Estados.FirstOrDefaultAsync(x => x.EstadoId == id);
+    }
+
+    public Task<bool> UpdateObject(Estados type)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Estados> AddObject(Estados type)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteObject(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> Exist(int id, string? nombre)
+    {
+        using var context = _contextFactory.CreateDbContext();
+        return await context.Estados
+            .AnyAsync(p => p.EstadoId != id && p.NombreEstado.ToLower().Equals(nombre.ToLower()));
+    }
+
+    public async Task<List<Estados>> GetObjectByCondition(Expression<Func<Estados, bool>> expression)
+    {
+        using var context = _contextFactory.CreateDbContext();
+        return await context.Estados
+            .AsNoTracking()
+            .Where(expression)
+            .ToListAsync();
+    }
+}
