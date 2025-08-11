c4f5732564fafd0b371d7b8e3de6dbd057909814
AngelixVR <Jose Gabriel@DESKTOP-SQIUKTF>
2025-08-11 10:33:57 -0400

    Add MetodoPagos class

---
+++ Models/MetodoPagos.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VRJewelers.Models;
+
+public class MetodoPagos
+{
+    [Key]
+    public int MetodoPagoId { get; set; }
+
+    [Required(ErrorMessage = "El Nombre es obligatorio")]
+    [RegularExpression("^[a-zA-Z ]+$", ErrorMessage = "El Nombre debe contener solo letras.")]
+    public string? Nombre { get; set; }
+}
