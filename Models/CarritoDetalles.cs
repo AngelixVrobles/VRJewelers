89eb81034a47aa59fc8d12b67242702d44288bdc
AngelixVrobles <angelixvrobles1234@outlook.com>
2025-08-15 19:28:24 -0400

    Add CarritoDetalles class with properties and annotations
    A new `CarritoDetalles` class was added to `CarritoDetalles.cs`.
    The class includes properties for managing cart details, such as
    `CarritoDetalleId`, `CarritoId`, `ProductoId`, `Cantidad`, `Precio`,
    and a calculated `Total` property. Key attributes like `[Key]` and
    `[NotMapped]` were used. Namespaces for models and annotations were
    also added to support the implementation.

---
+++ Models/CarritoDetalles.cs
@@ -0,0 +1,23 @@
+﻿using VrJewelers.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using VRJewelers.Models;
+
+public class CarritoDetalles
+{
+	[Key]
+	public int CarritoDetalleId { get; set; }
+
+	public int CarritoId { get; set; }
+	public Carrito? Carrito { get; set; }
+
+	public int ProductoId { get; set; }
+	public Productos? Producto { get; set; }
+
+	public int Cantidad { get; set; }
+
+	public float Precio { get; set; }
+
+	[NotMapped]
+	public float Total => Cantidad * Precio;
+}
