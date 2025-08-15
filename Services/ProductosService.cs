4269ba83ed5af34184f2537d15b93a78dbbf34b1
AngelixVrobles <angelixvrobles1234@outlook.com>
2025-08-15 19:30:10 -0400

    Refactor services and improve method organization
    Removed unused methods from `IdentityUserService` and refactored
    `ProductosService` with consistent naming and functionality.
    Replaced old methods with new, clearer ones like `Guardar`,
    `Insertar`, and `Modificar`. Simplified database context usage
    and improved code readability with `AsNoTracking` and better
    state management.

--- Services/ProductosService.cs
+++ Services/ProductosService.cs
@@ -1,56 +1,49 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System.Linq.Expressions;
-using System;
 using VRJewelers.Data;
 using VRJewelers.Models;
 
 namespace VRJewelers.Services;
 
-public class ProductosService(IDbContextFactory<ApplicationDbContext> contextFactory) : Productos
+public class ProductosService(IDbContextFactory<ApplicationDbContext> contextFactory)
 {
-    private readonly IDbContextFactory<ApplicationDbContext> _contextFactory = contextFactory;
-
-    public async Task<Productos> GetObject(int id)
+	public async Task<bool> Guardar(Productos producto)
     {
-        using var context = _contextFactory.CreateDbContext();
-        return await context.Productos.FindAsync(id);
+        if (!await Existe(producto.ProductoId))
+            return await Insertar(producto);
+        else
+            return await Modificar(producto);
     }
 
-    public async Task<List<Productos>> GetAllObject()
+	private async Task<bool> Insertar(Productos producto)
     {
-        using var context = _contextFactory.CreateDbContext();
-        return await context.Productos
-            .Include(c => c.Tipo)
-            .ToListAsync();
-    }
-
-    public async Task<Productos> AddObject(Productos producto)
-    {
-        using var context = _contextFactory.CreateDbContext();
+        using var context = contextFactory.CreateDbContext();
         context.Productos.Add(producto);
-        await context.SaveChangesAsync();
-        return producto;
+		return await context.SaveChangesAsync() > 0;
     }
 
-    public async Task<bool> UpdateObject(Productos producto)
+    private async Task<bool> Modificar(Productos producto)
     {
-        using var context = _contextFactory.CreateDbContext();
+        using var context = contextFactory.CreateDbContext();
         context.Productos.Update(producto);
-        var modificado = await context.SaveChangesAsync() > 0;
-        context.Entry(producto).State = EntityState.Modified;
-        return modificado;
+        return await context.SaveChangesAsync() > 0;
     }
 
-    public async Task<Productos?> Search(int productoId)
+    public async Task<Productos?> BuscarPorId(int productoId)
     {
-        using var context = _contextFactory.CreateDbContext();
-        return await context.Productos.AsNoTracking().FirstOrDefaultAsync(a => a.ProductoId == productoId);
+        using var context = contextFactory.CreateDbContext();
+        return await context
+            .Productos
+            .AsNoTracking()
+            .FirstOrDefaultAsync(a => a.ProductoId == productoId);
     }
 
-    public async Task<bool> DeleteObject(int id)
+    public async Task<bool> EliminarPorId(int id)
     {
-        using var context = _contextFactory.CreateDbContext();
-        var producto = await context.Productos.FindAsync(id);
+        using var context = contextFactory.CreateDbContext();
+        var producto = await context
+                            .Productos
+                            .FindAsync(id);
         if (producto == null)
         {
             return false;
@@ -59,19 +52,19 @@ public class ProductosService(IDbContextFactory<ApplicationDbContext> contextFac
         return await context.SaveChangesAsync() > 0;
     }
 
-    public async Task<bool> Exist(int id, string nombre)
+	private async Task<bool> Existe(int id)
     {
-        using var context = _contextFactory.CreateDbContext();
+        using var context = contextFactory.CreateDbContext();
         return await context.Productos
-            .AnyAsync(p => p.ProductoId != id && p.Nombre.ToLower().Equals(nombre.ToLower()));
+            .AnyAsync(p => p.ProductoId != id);
     }
 
-    public async Task<List<Productos>> GetObjectByCondition(Expression<Func<Productos, bool>> expression)
+    public async Task<List<Productos>> ListarFiltrador(Expression<Func<Productos, bool>> criterio)
     {
-        using var context = _contextFactory.CreateDbContext();
+        using var context = contextFactory.CreateDbContext();
         return await context.Productos
             .AsNoTracking()
-            .Where(expression)
+            .Where(criterio)
             .ToListAsync();
     }
 }
